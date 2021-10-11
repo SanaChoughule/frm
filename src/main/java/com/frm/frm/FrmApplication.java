63d4ea625448bbc2ca041d1805102375e09cfd60
Sana Choughule <Sana.Choughule@accelya.local>
2021-10-11 22:10:48 +0530

    Project scaffolding created

---
+++ src/main/java/com/frm/frm/FrmApplication.java
@@ -0,0 +1,13 @@
+package com.frm.frm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frm/frm/FrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frm.frm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
